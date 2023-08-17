ce14fa4218a7e7ffe7595dcfdc4c7a8b777386a4
Xiaoran <xc2422@columbia.edu>
2023-08-17 06:56:39 -0700

    rewrote login page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./user.js');
 const revenueRoutes = require('./revenue');
 const expenseRoutes = require('./expense');
-const createBudgetRoutes = require('../dashboard.js');
+// const createBudgetRoutes = require('../dashboard.js');
 const budgetRoutes = require('./budget');
 const sessionRoutes = require('./session');
 
@@ -10,7 +10,7 @@ const sessionRoutes = require('./session');
 router.use('/user', userRoutes);
 router.use('/revenue', revenueRoutes);
 router.use('/expense', expenseRoutes);
-router.use('/create', createBudgetRoutes);
+// router.use('/create', createBudgetRoutes);
 router.use('/budget', budgetRoutes);
 router.use('/session', sessionRoutes);
 
