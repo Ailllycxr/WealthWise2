4db6ec4fccdc501dc1e02291f4e7d290a832487f
Xiaoran <xc2422@columbia.edu>
2023-08-17 23:32:49 -0700

    adding readme

--- seeds/createUsersIncomes.js
+++ seeds/createUsersIncomes.js
@@ -3,7 +3,6 @@ const { User, Income } = require("../models");
 
 const createUsersIncomes = async () => {
     const users = await User.findAll();
- 
     for (const user of users) {
       let months = ['2023-01', '2023-02','2023-3','2023-4','2023-5','2023-6','2023-7','2023-8','2023-9','2023-10'];
       const income = months.map((month) => ({
